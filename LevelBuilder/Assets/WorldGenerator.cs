b22ee943d1a68005497ba294cca93fa6ae0fc567
Kjip <kjipgamer@gmail.com>
2019-06-10 17:44:32 +0200

    Fixed prefabs

--- LevelBuilder/Assets/WorldGenerator.cs
+++ LevelBuilder/Assets/WorldGenerator.cs
@@ -30,21 +30,23 @@ public class WorldGenerator : MonoBehaviour
 
         for (int i = 0; i < spawnAmount; i++)
         {
-             for (int b = 0; b < Pieces.Length; b++)
-              {
-            for (int j = 0; j < Pieces[i].transform.childCount; j++)
-            {
-                int r = Random.Range(0, 2);
-                Debug.Log(r);
-                if(r == 0)
-                    Pieces[i].transform.GetChild(j).GetComponent<Renderer>().material = light;
-                if(r == 1)
-                    Pieces[i].transform.GetChild(j).GetComponent<Renderer>().material = dark;
-                }
-            }
+             
 
-        Instantiate(Pieces[Random.Range(0,Pieces.Length)], spawnLocation, Quaternion.identity);
-        spawnLocation += new Vector3(5f, 0f, 0f);
+        GameObject tile = Instantiate(Pieces[Random.Range(0,Pieces.Length)], spawnLocation, Quaternion.identity);
+        tile.transform.eulerAngles = new Vector3(0, 90f, 0f);
+        tile.name = tile.name.Substring(0, tile.name.Length - 7);
+
+        string[] tileWalls = tile.name.Split('-');
+
+        if(tileWalls[0] == "0"){
+            spawnLocation += new Vector3(10f, 0f, 0f);
+        }
+        else
+        {
+            spawnLocation += new Vector3(5f, 0,0);
+        }
+
+        
             }
     }
 

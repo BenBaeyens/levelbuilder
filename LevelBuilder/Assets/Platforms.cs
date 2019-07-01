6023f328cad09c5d09beb7f34c0cf710ddb0f313
Kjip <kjipgamer@gmail.com>
2019-07-01 18:28:24 +0200

    Fixed prefab

---
+++ LevelBuilder/Assets/Platforms.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platforms : MonoBehaviour
+{
+
+
+
+    #region Methods
+
+    void Start()
+    {
+        
+    }
+
+    #endregion
+}
